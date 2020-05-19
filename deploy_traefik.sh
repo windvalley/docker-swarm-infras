734c36aabc366edf1becd6e176e6af9bfe332ff2
windvalley <i@sre.im>
2020-05-19 18:11:35 +0800

    Fix some env variables not exported bug.

--- deploy_traefik.sh
+++ deploy_traefik.sh
@@ -3,19 +3,18 @@
 set -e
 
 
-export \
-STACK_NAME=traefik-consul \
-# 3 or 5, not more. if you have a single node, set 0
-CONSUL_REPLICAS=3 \
-# the value number is equal to the count of swarm mode managers.
-# if you just have a single manager node, set 1
-TRAEFIK_REPLICAS=3 \
-# id of the current manager node
-NODE_ID=$(docker info -f '{{.Swarm.NodeID}}')
-
 while read -r var;do
     export $var
 done < <(grep -Ev '^#|^$' .env)
+
+export STACK_NAME=traefik-consul
+# 3 or 5, not more. if you have a single node, set 0
+export CONSUL_REPLICAS=3
+# the value number is equal to the count of swarm mode managers.
+# if you just have a single manager node, set 1
+export TRAEFIK_REPLICAS=3
+# id of the current manager node
+export NODE_ID=$(docker info -f '{{.Swarm.NodeID}}')
 
 
 docker network create --driver=overlay $TRAEFIK_NETWORK
