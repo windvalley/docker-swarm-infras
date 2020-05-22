415a2ce167ad313a8d78c48e44d35ec6cdc1c047
windvalley <i@sre.im>
2020-05-22 15:41:07 +0800

    Add .env file example.

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# deploy all
+
+set -e
+
+
+ROOT_DIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+cd $ROOT_DIR
+
+echo "Start deploying Traefik ..."
+./deploy_traefik.sh
+
+echo "Start deploying Registry ..."
+./deploy_registry.sh
+
+echo "Start deploying Swarmprom ..."
+./deploy_swarmprom.sh
+
+echo "Start deploying Swarmpit ..."
+./deploy_swarmpit.sh
+
+echo "Start deploying Portainer ..."
+./deploy_portainer.sh
